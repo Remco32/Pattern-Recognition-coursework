d407299c3d9cb3f573b0f0395de5956774fdf60d
Remco32 <remco32@gmail.com>
2018-12-17 13:33:41 +0100

    Hey this might work

--- Lab 4/myhough.m
+++ Lab 4/myhough.m
@@ -1,19 +1,25 @@
 % myHough function
-function [H,theta,rho]= myhough(edgemap)
-diag = hypot(size(edgemap,1),size(edgematp,2))
-H = zeros(2*diag,180);%size H from -diagonal  to diagonal and -90 and 90
-for t = 1:length(edgemap)
-    if edgemap(t) == 1
-    end
-end
-        
+function [H]= myhough(edgemap)
+
 % Quantize the parameter space (min and max values for rho and theta)
+theta_min = -90 + 90; %degrees; added 90 to avoid negative index values in matrix
+theta_max = 90 + 90;
+rho_max = hypot(size(edgemap,1),size(edgematp,2));
+rho_min = 1;
 
 % Initialize accumulator on the matrix representation of the input to zero
-favouritecolor = [25,47,256]
-% Loop through black (and/or?) white points in the image
-%%% Function from slides here
+H = zeros(2*diag,180); %size H from -diagonal  to diagonal and -90 and 90
 
-% Find local maxima in the accumulator
-%%% See formula slides
-    
+% Look for white pixels on the edgemap
+for t = 1:length(edgemap)
+    if edgemap(t) == 1
+        %%% Function from slides here
+        for theta = theta_min:theta_max
+            rho = x * cos(theta) + y * sin(theta);
+            H(theta, rho) = H(theta,rho) + 1;
+        end
+        % Find local maxima in the accumulator
+        %%% See formula slides
+    end
+end
+
