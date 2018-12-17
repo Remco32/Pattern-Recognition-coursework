ebb1330b9b19db626d16570272f77002184e5e5c
Remco32 <remco32@gmail.com>
2018-12-17 13:12:47 +0100

    Cool stuff:

--- Lab 4/myhough.m
+++ Lab 4/myhough.m
@@ -4,6 +4,8 @@ diag = hypot(size(edgemap,1),size(edgematp,2))
 H = zeros(2*diag,180);%size H from -diagonal  to diagonal and -90 and 90
 for t = 1:length(edgemap)
     if edgemap(t) == 1
+    end
+end
         
 % Quantize the parameter space (min and max values for rho and theta)
 
@@ -14,4 +16,4 @@ for t = 1:length(edgemap)
 
 % Find local maxima in the accumulator
 %%% See formula slides
-    end
+    
