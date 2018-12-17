0a83e5cc499326052c2387a8c8d5249878614c18
HuhuderUhu <45939344+HuhuderUhu@users.noreply.github.com>
2018-12-17 13:10:56 +0100

    Update myhough.m
    hey

--- Lab 4/myhough.m
+++ Lab 4/myhough.m
@@ -1,5 +1,10 @@
 % myHough function
-
+function [H,theta,rho]= myhough(edgemap)
+diag = hypot(size(edgemap,1),size(edgematp,2))
+H = zeros(2*diag,180);%size H from -diagonal  to diagonal and -90 and 90
+for t = 1:length(edgemap)
+    if edgemap(t) == 1
+        
 % Quantize the parameter space (min and max values for rho and theta)
 
 % Initialize accumulator on the matrix representation of the input to zero
@@ -9,3 +14,4 @@
 
 % Find local maxima in the accumulator
 %%% See formula slides
+    end
