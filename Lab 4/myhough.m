2139d0ee9acd0e3c03aeb62ec28785190243095a
Remco32 <remco32@gmail.com>
2018-12-17 12:53:35 +0100

    Added comments to have a skeleton file

--- Lab 4/myhough.m
+++ Lab 4/myhough.m
@@ -1,1 +1,11 @@
 % myHough function
+
+% Quantize the parameter space (min and max values for rho and theta)
+
+% Initialize accumulator on the matrix representation of the input to zero
+
+% Loop through black (and/or?) white points in the image
+%%% Function from slides here
+
+% Find local maxima in the accumulator
+%%% See formula slides
