02a3f191258035c443857eff364ea5ff8e6dad6c
Remco32 <remco32@gmail.com>
2018-12-18 12:06:39 +0100

    Added images and updated code A3

--- Lab 4/lab4_ass3.m
+++ Lab 4/lab4_ass3.m
@@ -3,7 +3,7 @@ I  = rgb2gray(imread('chess.jpg'));
 BW = edge(I,'canny');
 [H,T,R] = hough(BW);
 
-P  = houghpeaks(H,15,'threshold',ceil(0.3*max(H(:)))); 
+P  = houghpeaks(H,15,'threshold',ceil(0.3*max(H(:))));
 
 x = T(P(:,2));
 
@@ -17,11 +17,30 @@ maxArray = [];
 xArray = [];
 yArray = [];
 
-%for 1:size(H)
-maximum = max(max(H))
-maxArray = [maxArray; maximum] % for col array
-[x,y]=find(H==maximum)
-xArray = [xArray, x];
-yArray = [yArray, y];
-%Remove from array
-H(x,y) = 0; % Why can't we do this the easy way Matlab?
+N = 15; % Amount of points chosen
+% Why can't we do this the easy way Matlab?
+for i = 1:N
+    maximum = max(max(H));
+    maxArray = [maxArray; maximum];
+    [x,y]=find(H==maximum);
+    
+    % For duplicates
+    x = x(1);
+    y = y(1);
+    
+    xArray(i) = x;
+    yArray(i) = y;
+    %Remove from array
+    H(x,y) = 0; 
+end
+
+%%
+%Scatterplot for Q3
+scatter(xArray, yArray)
+
+%%
+%Plot img with top N lines
+imshow(I)
+hold on
+%Plot lines
+plot(xArray,yArray)
